51cb10ff57c311c96d5270513e3ff0854e4c02c6
Thomas Sundberg <tsu@kth.se>
2019-02-26 12:30:29 +0200

    Updated the steps to more explicit verify the actual result compared to the expected

--- features/step_definitions/Steps.cpp
+++ features/step_definitions/Steps.cpp
@@ -4,30 +4,26 @@ using cucumber::ScenarioScope;
 
 struct ToDoListContext {
     ToDoList toDoList;
-    string expectedName;
-    string expectedLastItem;
 };
 
 GIVEN("^that Thomas is out of cat food$"){
     ScenarioScope<ToDoListContext> context;
-    string name = "Thomas' To Do List";
-
-    context->toDoList.setName(name);
-    context->expectedName = name;
+    context->toDoList.setName("Thomas To Do List");
 }
 
 WHEN("^he adds buy cat food to his todo list$"){
     ScenarioScope<ToDoListContext> context;
-    
-    string buyCatFood = "buy cat food";
-
-    context->toDoList.add(buyCatFood);
-    context->expectedLastItem = buyCatFood;
+    context->toDoList.add("buy cat food");
 }
 
 THEN("^should Thomas todo list contain buy cat food$"){
     ScenarioScope<ToDoListContext> context;
 
-    EXPECT_EQ(context->expectedName, context->toDoList.getName());
-    EXPECT_EQ(context->expectedLastItem, context->toDoList.getLast());
+    string expectedName = "Thomas To Do List";
+    string actualName = context->toDoList.getName();
+    EXPECT_EQ(expectedName, actualName);
+
+    string expectedItem = "buy cat food";
+    string actualItem = context->toDoList.getLast();
+    EXPECT_EQ(expectedItem, actualItem);
 }
